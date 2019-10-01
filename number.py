5087658f2e671005953ec2cc8bfea449e8b95305
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-10-01 18:34:36 +0000

    number.py added

---
+++ number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by Amir Mersad
+# Created on September 2019
+# This program checks if there is over there 30 students
+
+import random
+
+
+def main():
+    # This function checks if there is over than 30 students
+    number_random = random.randint(1, 5)  # a number between 1 and 5
+
+    # Input
+    number_user = int(input("Enter a number:  "))
+    print("")
+
+    # Process & Output
+    if number_user == number_random:
+        print("You are right! ", number_random)
+    else:
+        print("You are wrong!")
+
+
+if __name__ == "__main__":
+    main()
